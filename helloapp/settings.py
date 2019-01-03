c14871ece090b75419a80fefd635cb672840d57e
Justin Csukran <jcsukran@gogoair.com>
2019-01-03 11:21:02 -0700

    updated settings

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
